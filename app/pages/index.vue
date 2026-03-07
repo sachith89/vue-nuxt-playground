45bbe95724d1307cabf46807fa41940f502a62df
slakmal <slakmal@coveragex.com>
2026-03-07 20:31:12 +0530

    Refactor styles to use Tailwind utilities and add counters with `ref()` and `useState()` in home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,10 +4,22 @@ useHead({
   title: 'My cool dashboard'
 })
 
+const counter1 = ref(0)
+const counter2 = useState('counter2', () => 0)
+
 </script>
+
+
 <template>
   <h3 class="text-2xl font-red">Welcome to home page</h3>
   <div>
     <NuxtLink to="/about">About</NuxtLink>
   </div>
+
+  <p>Counter using ref() -> {{ counter1 }}. Click here to <Button class="underline" @click="counter1++">Increment</Button></p>
+
+
+  <p>Counter using useState() -> {{ counter2 }}. Click here to <Button class="underline" @click="counter2++">Increment</Button></p>
+
+
 </template>
